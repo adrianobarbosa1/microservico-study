e54862451ff069dbcac825fdd9480fa1c6b62f73
adriano barbosa <adriano.barbosa100@gmail.com>
2022-10-21 18:45:27 -0300

    add validation error

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express"
 import { json } from "body-parser"
-
+import "express-async-errors"
 import { conectarRouter } from "./routes/conectar"
+import { cadastrarRouter } from "./routes/cadastrar"
+import { errorHandler } from "./middlewares/errorHandler"
+import { NotFoundError } from "./errors/notFoundError"
 
 const app = express()
 app.use(json())
+
+app.use(cadastrarRouter)
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!")
--- ticketing/auth/src/routes/cadastrar.ts
+++ ticketing/auth/src/routes/cadastrar.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from "express"
+import { body, validationResult } from "express-validator"
+import { RequestValidationError } from "../errors/requestValidationError"
+import { DatabaseConnectError } from "../errors/databaseConnectionError"
 
 const router = express.Router()
 
-router.post("/api/usuarios/cadastrar", (req: Request, res: Response) => {
-  res.send("hi there")
-})
+router.post(
+  "/api/usuarios/cadastrar",
+  [
+    body("email").isEmail().withMessage("Deve ser um email válido"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Senha deve ter entre 4 e 20 caracteres"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array())
+    }
+
+    console.log("Criando usuario...")
+    throw new DatabaseConnectError()
+    res.send({})
+  }
+)
 
 export { router as cadastrarRouter }
